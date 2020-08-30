1d033f589c9d07d8c56d174555d0999561db3bc9
Haris Rahman <haris.rahman@larisin.id>
2020-08-30 10:39:11 +0700

    [CONSTANT] Add constant file

---
+++ src/constants/leagueList.js
@@ -0,0 +1,29 @@
+const leagueList = [
+  {
+    id: 'seriea',
+    name: 'Serie A',
+    logo: '/images/seriea.png',
+  },
+  {
+    id: 'liga',
+    name: 'La Liga',
+    logo: '/images/liga.png',
+  },
+  {
+    id: 'premierleague',
+    name: 'Premier League',
+    logo: '/images/premierleague.png',
+  },
+  {
+    id: 'bundesliga',
+    name: 'Bundesliga',
+    logo: '/images/bundesliga.png',
+  },
+  {
+    id: 'ligue1',
+    name: 'Ligue 1',
+    logo: '/images/ligue1.png',
+  },
+];
+
+export default leagueList;
